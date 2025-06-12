7c64638956bfef717081643626fec32c04d8f5e9
Dmitrii Kharitonov <dimds@fastmail.com>
2025-06-12 20:36:38 +0200

    Reexport handle_get_page_bundle in the handlers module

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,11 +1,12 @@
 # Reexport all handlers
 
-from .get import handle_get_document
+from .get import handle_get_document, handle_get_page_bundle
 from .list import handle_get_documents
 from .upload import handle_upload_document
 
 __all__ = [
     "handle_get_document",
     "handle_get_documents",
+    "handle_get_page_bundle",
     "handle_upload_document",
 ]
